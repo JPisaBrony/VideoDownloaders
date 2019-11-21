52ae780c5ec0c887fb1a772faebaf0d87508a6fc
JPisaBrony <jdpohl789@gmail.com>
2019-11-21 08:11:37 -0500

    + various fixes to thewatchcartoononline-downloader + fix for timing out when downloading + some cleanup

--- thewatchcartoononline-downloader.py
+++ thewatchcartoononline-downloader.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 import requests
 import js2py
 import base64
@@ -9,20 +11,22 @@ def atob(s):
 
 ctx = js2py.EvalJs({"atob": atob})
 
+EPISODE_URL_NAME = "star-vs-the-forces-of-evil"
 BASE_WEBPAGE_URL = "https://www.thewatchcartoononline.tv"
 BASE_DOWNLOAD_DIR = "episodes/"
 
-base_url = requests.get("https://www.thewatchcartoononline.tv/anime/dragon-ball-super", 'html.parser')
+base_url = requests.get(BASE_WEBPAGE_URL + "/anime/" + EPISODE_URL_NAME, 'html.parser')
 soup = BeautifulSoup(base_url.text, "html.parser")
+retries = Retry(total=10, backoff_factor=3)
 for eps in reversed(soup.findAll("div", {"class": "cat-eps"})):
     ep = eps.find("a")
     ep_url = ep['href']
     print "processing " + ep_url
-
-    if os.path.isfile(BASE_DOWNLOAD_DIR + ep.text + ".mp4"):
+    
+    if os.path.isfile(BASE_DOWNLOAD_DIR + ep.text.replace("/", ".") + ".mp4"):
         print ep.text + " exists. skipping"
         continue
-
+    
     ep_page = requests.get(ep_url, 'html.parser')
     ep_soup = BeautifulSoup(ep_page.text, "html.parser")
     script = ep_soup.findAll("script")[5]
@@ -39,9 +43,12 @@ for eps in reversed(soup.findAll("div", {"class": "cat-eps"})):
     video_json = requests.get(video_get_link, headers={"x-requested-with": "XMLHttpRequest"}).json()
     video_real_link = video_json["server"] + "/getvid?evid=" + video_json["enc"]
     
-    with requests.get(video_real_link, stream=True, timeout=10) as r:
+    with requests.Session() as s:
+        s.mount('https://', HTTPAdapter(max_retries=retries))
+        r = s.get(video_real_link, stream=True, timeout=30)
         r.raise_for_status()
-        with open(BASE_DOWNLOAD_DIR + ep.text + ".mp4", 'wb') as f:
+        print ep.text
+        with open(BASE_DOWNLOAD_DIR + ep.text.replace("/", ".") + ".mp4", 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
